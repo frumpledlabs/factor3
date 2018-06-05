4b0ef2f222af5facd3e1ea0c7cc9e347edecc6ea
Frumpled Biscuit <frumpledbiscuit@gmail.com>
2018-06-05 03:45:30 -0600

    Add test cases for conf loading

--- configure_test.go
+++ configure_test.go
@@ -55,3 +55,53 @@ func Test_SetRequiredVariableLoadsWithoutError(t *testing.T) {
 	err := Load(conf)
 	assert.Nil(t, err)
 }
+
+func Test_DefaultValueOverriden(t *testing.T) {
+	os.Setenv("DEFAULT_KEY_EXISTS", "OVERRIDDEN")
+	os.Unsetenv("DEFAULT_KEY_EXISTS")
+
+	conf := struct {
+		defaultKeyExists string `envDefault:"MISSING"`
+	}{}
+
+	Load(conf)
+
+	assert.Equal(t, "OVERRIDDEN", conf.defaultKeyExists)
+}
+
+func Test_DefaultValueIsOveriddenWhenEmptyValueSet(t *testing.T) {
+	os.Setenv("DEFAULT_KEY_IS_EMPTY_STRING", "")
+	defer os.Unsetenv("DEFAULT_KEY_IS_EMPTY_STRING")
+
+	conf := struct {
+		defaultKeyIsEmptyString string `envDefault:"EMPTY"`
+	}{}
+
+	Load(conf)
+
+	assert.Equal(t, "", conf.defaultKeyIsEmptyString)
+}
+
+func Test_DefaultValuePersistsWhenEnvVariableNotSet(t *testing.T) {
+	conf := struct {
+		defaultKeySet string `envDefault:"DEFAULT"`
+	}{}
+
+	Load(conf)
+
+	assert.Equal(t, "DEFAULT", conf.defaultKeySet)
+}
+
+func Test_RequiredWithDefaultDoesNotErrorWhenNotSet(t *testing.T) {
+	conf := struct {
+		requiredWithDefault string `env:"required" envDefault:"DEFAULT"`
+	}{}
+
+	err := Load(conf)
+
+	assert.Nil(t, err)
+}
+
+func Test_PrintKeys(t *testing.T) {
+	assert.Fail(t, "Not yet implemented")
+}

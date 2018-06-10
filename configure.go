cedb6a989c65cf44c8453551e7552a13e93d130c
Frumpled Biscuit <frumpledbiscuit@gmail.com>
2018-06-10 15:24:26 -0600

    Update Load func description and reorder field reflection

--- configure.go
+++ configure.go
@@ -8,8 +8,8 @@ import (
 	"strconv"
 )
 
-// Loads configuration into the given variable, assuming all values are
-// optional, unless otherwise tagged.
+// Loads environment into given configuration variable, using specific
+// tags to determine requirements, values, and behavior.
 func Load(input interface{}) error {
 	if reflect.TypeOf(input).Kind() != reflect.Ptr {
 		return errors.New("Expected a struct pointer")
@@ -65,6 +65,14 @@ func setField(prefix string, field reflect.Value, fieldType reflect.StructField)
 			if val, err := strconv.ParseBool(value); err == nil {
 				field.Set(reflect.ValueOf(val).Convert(field.Type()))
 			}
+		case reflect.Float32:
+			if val, err := strconv.ParseFloat(value, 32); err == nil {
+				field.Set(reflect.ValueOf(float32(val)).Convert(field.Type()))
+			}
+		case reflect.Float64:
+			if val, err := strconv.ParseFloat(value, 64); err == nil {
+				field.Set(reflect.ValueOf(val).Convert(field.Type()))
+			}
 		case reflect.Int:
 			if val, err := strconv.ParseInt(value, 10, 64); err == nil {
 				field.Set(reflect.ValueOf(int(val)).Convert(field.Type()))
@@ -83,38 +91,6 @@ func setField(prefix string, field reflect.Value, fieldType reflect.StructField)
 			}
 		case reflect.Int64:
 			if val, err := strconv.ParseInt(value, 10, 64); err == nil {
-				field.Set(reflect.ValueOf(val).Convert(field.Type()))
-			}
-		case reflect.Uint:
-			if val, err := strconv.ParseUint(value, 10, 64); err == nil {
-				field.Set(reflect.ValueOf(uint(val)).Convert(field.Type()))
-			}
-		case reflect.Uint8:
-			if val, err := strconv.ParseUint(value, 10, 8); err == nil {
-				field.Set(reflect.ValueOf(uint8(val)).Convert(field.Type()))
-			}
-		case reflect.Uint16:
-			if val, err := strconv.ParseUint(value, 10, 16); err == nil {
-				field.Set(reflect.ValueOf(uint16(val)).Convert(field.Type()))
-			}
-		case reflect.Uint32:
-			if val, err := strconv.ParseUint(value, 10, 32); err == nil {
-				field.Set(reflect.ValueOf(uint32(val)).Convert(field.Type()))
-			}
-		case reflect.Uint64:
-			if val, err := strconv.ParseUint(value, 10, 64); err == nil {
-				field.Set(reflect.ValueOf(val).Convert(field.Type()))
-			}
-		case reflect.Uintptr:
-			if val, err := strconv.ParseUint(value, 10, 64); err == nil {
-				field.Set(reflect.ValueOf(uintptr(val)).Convert(field.Type()))
-			}
-		case reflect.Float32:
-			if val, err := strconv.ParseFloat(value, 32); err == nil {
-				field.Set(reflect.ValueOf(float32(val)).Convert(field.Type()))
-			}
-		case reflect.Float64:
-			if val, err := strconv.ParseFloat(value, 64); err == nil {
 				field.Set(reflect.ValueOf(val).Convert(field.Type()))
 			}
 		case reflect.String:
@@ -142,7 +118,10 @@ func setField(prefix string, field reflect.Value, fieldType reflect.StructField)
 }
 
 func isZeroValue(field reflect.Value) bool {
-	return reflect.DeepEqual(reflect.Zero(field.Type()).Interface(), field.Interface())
+	return reflect.DeepEqual(
+		reflect.Zero(field.Type()).Interface(),
+		field.Interface(),
+	)
 }
 
 // Returns a slice of all field names' calculated env variable name

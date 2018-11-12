12851f52384476109950191ceb44f7f0f206857e
Jonathan Brown <jonathan.brown@vertafore.com>
2018-11-12 16:41:45 -0600

    Update environment type interface and examples

---
+++ examples/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/frumpled/factor3"
+)
+
+func main() {
+	fmt.Println("vim-go")
+
+	os.Setenv("EXISTING_VARIABLE_VALUE", "PASSED")
+	defer os.Unsetenv("EXISTING_VARIABLE_VALUE")
+
+	config := struct {
+		ExistingVariableValue    string
+		NonExistingVariableValue string
+	}{
+		ExistingVariableValue:    "",
+		NonExistingVariableValue: "",
+	}
+
+	err := factor3.ReadEnvironment().
+		WithVariablePrefix("").
+		ReadInto(&config)
+
+	fmt.Println("Err:", err)
+	fmt.Println("Config:", config)
+
+}

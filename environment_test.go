d245129fd2a6e32cb5e2575e8244456f39e40fd2
Jonathan Brown <frumpledbiscuit@gmail.com>
2018-12-08 22:51:33 -0700

    Add test for all primitive types

--- environment_test.go
+++ environment_test.go
@@ -56,3 +56,48 @@ func Test_LoadVariablesWithPrefixPopluatesExpectedValues(t *testing.T) {
 
 	assert.Equal(t, expectedStringVal, config.StringVar)
 }
+
+func Test_LoadVariablesOfAllSupportedTypesWithoutError(t *testing.T) {
+	os.Setenv("STRING_VAR", "STRING")
+	os.Setenv("INT_VAR", "1")
+	os.Setenv("INT8VAR", "8")
+	os.Setenv("INT16VAR", "16")
+	os.Setenv("INT32VAR", "32")
+	os.Setenv("INT64VAR", "64")
+	os.Setenv("FLOAT32VAR", "32.32")
+	os.Setenv("FLOAT64VAR", "64.64")
+
+	defer os.Unsetenv("STRING_VAR")
+	defer os.Unsetenv("INT_VAR")
+	defer os.Unsetenv("INT8VAR")
+	defer os.Unsetenv("INT16VAR")
+	defer os.Unsetenv("INT32VAR")
+	defer os.Unsetenv("INT64VAR")
+	defer os.Unsetenv("FLOAT32VAR")
+	defer os.Unsetenv("FLOAT64VAR")
+
+	config := struct {
+		StringVar  string
+		Float32Var float32
+		Float64Var float64
+		IntVar     int
+		Int8Var    int8
+		Int16Var   int16
+		Int32Var   int32
+		Int64Var   int64
+	}{}
+
+	err := LoadEnvironment().
+		Into(&config)
+
+	require.Nil(t, err)
+
+	assert.Equal(t, "STRING", config.StringVar)
+	assert.Equal(t, int(1), config.IntVar)
+	assert.Equal(t, int8(8), config.Int8Var)
+	assert.Equal(t, int16(16), config.Int16Var)
+	assert.Equal(t, int32(32), config.Int32Var)
+	assert.Equal(t, int64(64), config.Int64Var)
+	assert.Equal(t, float32(32.32), config.Float32Var)
+	assert.Equal(t, float64(64.64), config.Float64Var)
+}

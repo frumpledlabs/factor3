89fd1d8f846554e4dce674f7f2d3031a8e267dfc
Jonathan Brown <frumpledbiscuit@gmail.com>
2019-09-13 16:28:15 -0500

    Add new logger type abstracting external logging library

--- logger.go
+++ logger.go
@@ -4,12 +4,43 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var log *logrus.Logger
+// Logger is a simple logger type
+// that outputs JSON logs
+// and an optional map of custom fields & values
+type Logger interface {
+	Info(string, map[string]interface{})
+	Debug(string, map[string]interface{})
+	Warn(string, map[string]interface{})
+	Fatal(string, map[string]interface{})
+}
 
-func init() {
+type logger struct {
+	*logrus.Logger
+}
+
+// NewLogger returns a new logger w/ default configs
+func NewLogger() Logger {
 	logrusLogger := logrus.New()
 	logrusLogger.SetFormatter(&logrus.JSONFormatter{})
 	logrusLogger.SetLevel(logrus.InfoLevel)
 
-	log = logrusLogger
+	return logger{
+		logrusLogger,
+	}
+}
+
+func (l logger) Debug(msg string, fields map[string]interface{}) {
+	l.WithFields(fields).Debug(msg)
+}
+
+func (l logger) Info(msg string, fields map[string]interface{}) {
+	l.WithFields(fields).Info(msg)
+}
+
+func (l logger) Warn(msg string, fields map[string]interface{}) {
+	l.WithFields(fields).Warn(msg)
+}
+
+func (l logger) Fatal(msg string, fields map[string]interface{}) {
+	l.WithFields(fields).Fatal(msg)
 }
